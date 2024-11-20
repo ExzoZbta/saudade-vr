7dca1ac52ad18f6aeaa5f51c4d32c97fe9aa58b4
Richard Corrente <exzozbta@gmail.com>
2024-11-20 03:56:04 -0500

    implement mirror game mechanic
    - still need to fine tune game object detection via mirror cam radius

---
+++ Assets/_Course Library/Scripts/Actions/MirrorReveal.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorReveal : MonoBehaviour
+{
+    [Header("Mirror Settings")]
+    public Camera mirrorCamera;
+    public float checkRadius = 5f;
+    public LayerMask revealableLayer;
+
+    [Header("Optional Settings")]
+    public float fadeSpeed = 2f;
+    public bool useFadeEffect = true;
+
+    private void Update()
+    {
+        // Find all objects on the revealable layer within radius
+        Collider[] hitColliders = Physics.OverlapSphere(mirrorCamera.transform.position, checkRadius, revealableLayer);
+
+        foreach (var hitCollider in hitColliders)
+        {
+            // Check if object is within mirror camera's view
+            Vector3 directionToTarget = (hitCollider.transform.position - mirrorCamera.transform.position).normalized;
+            float angle = Vector3.Angle(mirrorCamera.transform.forward, directionToTarget);
+
+            // Get the renderer component
+            Renderer renderer = hitCollider.GetComponent<Renderer>();
+            if (renderer == null) continue;
+
+            // Get or create material property block to modify properties efficiently
+            MaterialPropertyBlock propBlock = new MaterialPropertyBlock();
+            renderer.GetPropertyBlock(propBlock);
+
+            // Check if object is within mirror camera's field of view
+            if (angle <= mirrorCamera.fieldOfView * 0.5f)
+            {
+                // Perform raycast to check for obstacles
+                RaycastHit hit;
+                if (Physics.Raycast(mirrorCamera.transform.position, directionToTarget, out hit, checkRadius))
+                {
+                    if (hit.collider == hitCollider)
+                    {
+                        // Object is visible in mirror
+                        if (useFadeEffect)
+                        {
+                            float currentAlpha = propBlock.GetFloat("_Alpha");
+                            float newAlpha = Mathf.Min(currentAlpha + Time.deltaTime * fadeSpeed, 1f);
+                            propBlock.SetFloat("_Alpha", newAlpha);
+                        }
+                        else
+                        {
+                            propBlock.SetFloat("_Alpha", 1f);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // Object is not in mirror view
+                if (useFadeEffect)
+                {
+                    float currentAlpha = propBlock.GetFloat("_Alpha");
+                    float newAlpha = Mathf.Max(currentAlpha - Time.deltaTime * fadeSpeed, 0f);
+                    propBlock.SetFloat("_Alpha", newAlpha);
+                }
+                else
+                {
+                    propBlock.SetFloat("_Alpha", 0f);
+                }
+            }
+
+            // Apply the property block to the renderer
+            renderer.SetPropertyBlock(propBlock);
+        }
+    }
+}
---
+++ Assets/_Course Library/Scripts/Conditions/SeeMirror.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeMirror : MonoBehaviour
+{
+    Renderer m_Renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Renderer.isVisible)
+        {
+            Debug.Log("Object is visible");
+        }
+        else Debug.Log("Object is no longer visible");
+    }
+}
